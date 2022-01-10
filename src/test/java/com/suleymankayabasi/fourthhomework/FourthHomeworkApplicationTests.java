27e02e96b978e41ceabb4c8afcbca133e252a9e7
Süleyman Kayabaşı <82765835+suleymankayabasi@users.noreply.github.com>
2022-01-11 00:16:56 +0300

    Initial Commit

---
+++ src/test/java/com/suleymankayabasi/fourthhomework/FourthHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suleymankayabasi.fourthhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourthHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
